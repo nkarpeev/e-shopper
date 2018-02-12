c07d86c0c9d375e86166b147c26f5bd347e2515e
nekarpeev <32749922+nekarpeev@users.noreply.github.com>
2018-02-12 15:42:53 +0400

    Update footer.php

--- view/layouts/footer.php
+++ view/layouts/footer.php
@@ -3,8 +3,7 @@
             <div class="footer-bottom">
                 <div class="container">
                     <div class="row">
-                        <p class="pull-left">Copyright © 2015</p>
-                        <p class="pull-right">Курс PHP Start</p>
+                        <p class="pull-left">Copyright © 2017</p>
                     </div>
                 </div>
             </div>
